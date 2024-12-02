558d06613633f1571737c04bf36311cdcbad62df
scriptKid <gopikrishnan0045@gmail.com>
2024-12-02 21:56:34 +0530

    choose design vars

--- FRONTEND/src/assets/components/createArea.jsx
+++ FRONTEND/src/assets/components/createArea.jsx
@@ -1,7 +1,12 @@
 function CreateArea() {
     return <>
         <section>
-            <h1>Create Area</h1>
+            <h1>H1</h1>
+            <h2>H2</h2>
+            <h3>H3</h3>
+            <p>P</p>
+            <span>Span</span>
+            <nav>Nav</nav>
         </section>
     </>
 }
